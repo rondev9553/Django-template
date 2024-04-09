eaab24b95f46c639c3d98694082ca2d4cd902c85
BOB0320 <diamuji1@gmail.com>
2024-04-09 19:22:51 -0400

    ffmpeg install

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "==========Start start command=========="
+apt-get update
+apt-get install ffmpeg
+ffmpeg -version
+echo "==========ffmpeg version above=========="
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
