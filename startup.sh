702e72a270214d1c5844379ff76bfe465be23b0e
BOB0320 <diamuji1@gmail.com>
2024-04-09 19:38:40 -0400

    install ffmpeg aws linux

--- startup.sh
+++ startup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 echo "==========Start start command=========="
-apt-get update
-apt-get install ffmpeg
+if [ ! -f /usr/local/bin/ffmpeg ]
+then
+  wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+  tar xvf ffmpeg-release-amd64-static.tar.xz
+  mv ffmpeg-*-amd64-static/ff* /usr/local/bin/
+fi
 ffmpeg -version
 echo "==========ffmpeg version above=========="
 python3 -m venv .venv
 source .venv/bin/activate
+ffmpeg -version
+echo "==========ffmpeg version33333 above=========="
 pip3 install -r requirements.txt
 python3 manage.py collectstatic 
 gunicorn --workers 2 testsite.wsgi
