02888af8315be3ae6fa33cd1b4f5bee7d54d2067
BOB0320 <diamuji1@gmail.com>
2024-04-10 03:19:19 -0400

    update ssh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo "==========Start start command=========="
-sudo su -
-cd /usr/local/bin
-mkdir ffmpeg
-cd ffmpeg
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-tar -xzf ffmpeg-git-amd64-static.tar.xz
-./ffmpeg -version
+sudo yum update
+sudo yum search wget
+sudo yum install wget
+sudo yum search tar
+sudo yum install tar
+sudo wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+sudo tar -xvf ffmpeg-git-amd64-static.tar.xz
+sudo mkdir ffmpeg-git-20240301-amd64-static/ffmpeg
+sudo mv ffmpeg-git-20240301-amd64-static/ffmpeg /usr/local/bin/
+sudo chmod +x /usr/local/bin/ffmpeg
 ffmpeg -version
 echo "==========ffmpeg version above=========="
 python3 -m venv .venv
